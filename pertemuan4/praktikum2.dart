4a6a0fc5375f65c041d0a9ed50d6cc4ce8936341
AtthalaricNero <atthalaricnero@gmail.com>
2025-09-24 05:36:51 +0700

    praktikum 2 langkah 3

---
+++ pertemuan4/praktikum2.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  // var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
+  // print(halogens);
+
+  var names1 = <String>{};
+  Set<String> names2 = {};
+  var names3 = {};
+
+
+  print(names1);
+  print(names2);
+  print(names3);
+}
