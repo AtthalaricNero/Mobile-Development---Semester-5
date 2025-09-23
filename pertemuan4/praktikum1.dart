28aea9319e7f59e0f6f7ff72b4eda249bd056a99
AtthalaricNero <atthalaricnero@gmail.com>
2025-09-23 23:14:51 +0700

    Update praktikum1.dart to demonstrate List data type with new example values

--- pertemuan4/praktikum1.dart
+++ pertemuan4/praktikum1.dart
@@ -1,15 +1,10 @@
 // Eksperimen Tipe Data List
 
 void main(List<String> args) {
-  final list = [1, 2, 3];
-  assert(list.length == 3);
-  assert(list[1] == 2);
-  print(list.length);
-  print(list[1]);
-
-  list[1] = 1;
-  assert(list[1] == 1);
-  print(list[1]);
+  final list = List.filled(5, null);
+  list[1] = "Atthalaric Nero. M";
+  list[2] = "2341720215";
   
+  print(list);
 }
 
