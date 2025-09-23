76763d4cc75bed8adbe9d9a20f49d2556f3a18d6
AtthalaricNero <atthalaricnero@gmail.com>
2025-09-24 05:44:44 +0700

    praktikum 2 langkah 3 menambahkan nama dan nim menggunakan fungsi add dan addAll

--- pertemuan4/praktikum2.dart
+++ pertemuan4/praktikum2.dart
@@ -6,8 +6,12 @@ void main(List<String> args) {
   Set<String> names2 = {};
   var names3 = {};
 
+  names1.add("Atthalaric Nero. M");
+  names1.add("2341720215");
+  names2.addAll(["Atthalaric Nero. M", "2341720215"]);
 
-  print(names1);
-  print(names2);
-  print(names3);
+
+  print("Ini set names1 $names1");
+  print("Ini set names2 $names2");
+  print("Ini set names3 $names3");
 }
