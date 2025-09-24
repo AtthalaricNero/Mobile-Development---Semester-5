df911a47d4691724570a3f202ea2f3cf29625eb8
AtthalaricNero <atthalaricnero@gmail.com>
2025-09-24 12:10:54 +0700

    praktikum 5 langkah 3

--- pertemuan4/praktikum5.dart
+++ pertemuan4/praktikum5.dart
@@ -1,4 +1,12 @@
+(int, int) tukar((int, int) record) {
+  var (a, b) = record;
+  return (b, a);
+}
+
 void main(List<String> args) {
   var record = ('first', a: 2, b: true, 'last');
   print(record);
+
+  var record2 = tukar((3, 5));
+  print(record2);
 }
