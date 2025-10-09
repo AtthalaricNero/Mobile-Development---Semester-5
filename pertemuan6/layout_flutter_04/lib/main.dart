6bd0fc39b16235c3955510c6f1d0ddfa25d2e8e5
AtthalaricNero <atthalaricnero@gmail.com>
2025-10-09 16:58:18 +0700

    Praktikum 2

--- pertemuan6/layout_flutter_04/lib/main.dart
+++ pertemuan6/layout_flutter_04/lib/main.dart
@@ -5,55 +5,78 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 
+  @override
   Widget build(BuildContext context) {
     Widget titleSection = Container(
-  padding: const EdgeInsets.all(32),
-  child: Row(
-    children: [
-      Expanded(
-        /* soal 1*/
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            /* soal 2*/
-            Container(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: const Text(
-                'Wisata Gunung di Batu',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /* soal 1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /* soal 2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
                 ),
-              ),
+                Text(
+                  'Batu, Malang, Indonesia',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ],
             ),
-            Text(
-              'Batu, Malang, Indonesia',
-              style: TextStyle(color: Colors.grey),
-            ),
-          ],
-        ),
+          ),
+          /* soal 3*/
+          const Icon(Icons.star, color: Colors.yellow),
+          const Text('41'),
+        ],
       ),
-      /* soal 3*/
-      const Icon(
-       Icons.star,
-        color: Colors.yellow,
-      ),
-      const Text('41'),
-    ],
-  ),
-);
+    );
+
+    Color color = Theme.of(context).primaryColor;
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
     return MaterialApp(
       title: 'Flutter layout: Atthalaric Nero. M - 2341720215',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter layout demo'),
-        ),
-        body: Column(
-          children: [
-            titleSection,
-          ],
-        ),
+        appBar: AppBar(title: const Text('Flutter layout demo')),
+        body: Column(children: [
+          titleSection,
+          buttonSection,
+          ]),
       ),
     );
   }
